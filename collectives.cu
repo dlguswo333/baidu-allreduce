b137ec10e12a1e14febf74f8565ac6ed51bb15e6
dlguswo333 <dlguswo333@skku.edu>
2021-02-23 13:38:53 +0000

    edit timer location

--- collectives.cu
+++ collectives.cu
@@ -306,13 +306,13 @@ void RingAllreduce(float* data, size_t length, float** output_ptr) {
     // segments with wraparound and send and recv from our neighbors and reduce
     // locally. At the i'th iteration, sends segment (rank - i) and receives
     // segment (rank - i - 1).
+    timer.start();
     for (int i = 0; i < size - 1; i++) {
         int recv_chunk = (rank - i - 1 + size) % size;
         int send_chunk = (rank - i + size) % size;
         float* segment_send = &(output[segment_ends[send_chunk] -
                                    segment_sizes[send_chunk]]);
 
-	timer.start();
 
         MPI_Irecv(buffer, segment_sizes[recv_chunk],
                 datatype, recv_from, 0, MPI_COMM_WORLD, &recv_req);
@@ -329,16 +329,16 @@ void RingAllreduce(float* data, size_t length, float** output_ptr) {
 
         reduce(segment_update, buffer, segment_sizes[recv_chunk]);
 
-        seconds=timer.seconds();
-        std::cout << "scatter-reduce: " << seconds << '\n';
     }
+    seconds=timer.seconds();
+    std::cout << "scatter-reduce: " << seconds << '\n';
     //timer.start();
     // Now start pipelined ring allgather. At every step, for every rank, we
     // iterate through segments with wraparound and send and recv from our
     // neighbors. At the i'th iteration, rank r, sends segment (rank + 1 - i)
     // and receives segment (rank - i).
+    timer.start();
     for (size_t i = 0; i < size_t(size - 1); ++i) {
-	timer.start();
         int send_chunk = (rank - i + 1 + size) % size;
         int recv_chunk = (rank - i + size) % size;
         // Segment to send - at every iteration we send segment (r+1-i)
@@ -353,10 +353,10 @@ void RingAllreduce(float* data, size_t length, float** output_ptr) {
                 segment_sizes[recv_chunk], datatype, recv_from,
                 0, MPI_COMM_WORLD, &recv_status);
 
-        seconds=timer.seconds();
-        std::cout << "allgather: " << seconds << '\n';
     }
 
+    seconds=timer.seconds();
+    std::cout << "allgather: " << seconds << '\n';
     //seconds=timer.seconds();
     //std::cout <<  "allreduce : " << seconds << '\n';
 
