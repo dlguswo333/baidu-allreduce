09cd4561e0a1387718164a67cc7b9459f9eb9f65
dlguswo333 <dlguswo333@skku.edu>
2021-03-04 06:24:32 +0000

    change iter

--- test/test.cpp
+++ test/test.cpp
@@ -171,7 +171,7 @@ int main(int argc, char** argv) {
 
     // Number of iterations to run for each buffer size.
     std::vector<size_t> iterations = {
-       1 
+       20
     };
 
     // Test on either CPU and GPU.
