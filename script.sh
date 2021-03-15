bf5b79c3abfffbe47338723a9457a7f2d7d21950
dlguswo333 <dlguswo333@skku.edu>
2021-03-15 06:37:44 +0000

    edit script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,28 @@
-printf "4\n\n" >> /data/out.txt
-for i in {1..40}
+printf "41\n\n" >> ~/thread.txt
+for i in {1..10}
 do
-mpirun --allow-run-as-root -np 4 -mca btl_tcp_if_exclude eno0,eno1 --tag-output -mca plm_rsh_args "-p 3333" -H localhost:4 allreduce-test cpu >> /data/out.txt
+mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:4,tb3-ib:1 allreduce-test cpu >> ~/thread.txt
 sleep 30s;
 done
 
-printf "22\n\n" >> /data/out.txt
-for i in {1..40}
+printf "32\n\n" >> ~/thread.txt
+for i in {1..10}
 do
-mpirun --allow-run-as-root -np 4 -mca btl_tcp_if_exclude eno0,eno1 --tag-output -mca plm_rsh_args "-p 3333" -H localhost:2,tb3-ib:2 allreduce-test cpu >> /data/out.txt
+mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:3,tb3-ib:2 allreduce-test cpu >> ~/thread.txt
 sleep 30s;
 done
 
-printf "22\n\n" >> /data/out.txt
-for i in {1..40}
+printf "23\n\n" >> ~/thread.txt
+for i in {1..10}
 do
-mpirun --allow-run-as-root -np 4 -mca btl_tcp_if_exclude eno0,eno1 --tag-output -mca plm_rsh_args "-p 3333" -H localhost:3,tb3-ib:1 allreduce-test cpu >> /data/out.txt
+mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:2,tb3-ib:3 allreduce-test cpu >> ~/thread.txt
 sleep 30s;
 done
+
+printf "14\n\n" >> ~/thread.txt
+for i in {1..10}
+do
+mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:1,tb3-ib:4 allreduce-test cpu >> ~/thread.txt
+sleep 30s;
+done
+
