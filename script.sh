d7344da8efbe40c3d2907216d554dafc9535a666
dlguswo333 <dlguswo333@skku.edu>
2021-03-26 08:30:50 +0000

    init script.sh

--- script.sh
+++ script.sh
@@ -1,28 +1,34 @@
-printf "41\n\n" >> /data/out.txt
+printf "51\n\n" >> ~/single.txt
 for i in {1..10}
 do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:4,tb3-ib:1 allreduce-test cpu >> /data/out.txt
+mpirun -n 6 -machine m51 -iface enp216s0 ./allreduce-test cpu >> ~/single.txt
 sleep 30s;
 done
 
-printf "32\n\n" >> /data/out.txt
+printf "42\n\n" >> ~/single.txt
 for i in {1..10}
 do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:3,tb3-ib:2 allreduce-test cpu >> /data/out.txt
+mpirun -n 6 -machine m42 -iface enp216s0 ./allreduce-test cpu >> ~/single.txt
 sleep 30s;
 done
 
-printf "23\n\n" >> /data/out.txt
+printf "33\n\n" >> ~/single.txt
 for i in {1..10}
 do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:2,tb3-ib:3 allreduce-test cpu >> /data/out.txt
+mpirun -n 6 -machine m33 -iface enp216s0 ./allreduce-test cpu >> ~/single.txt
 sleep 30s;
 done
 
-printf "14\n\n" >> /data/out.txt
+printf "24\n\n" >> ~/single.txt
 for i in {1..10}
 do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:1,tb3-ib:4 allreduce-test cpu >> /data/out.txt
+mpirun -n 6 -machine m24 -iface enp216s0 ./allreduce-test cpu >> ~/single.txt
 sleep 30s;
 done
 
+printf "15\n\n" >> ~/single.txt
+for i in {1..10}
+do
+mpirun -n 6 -machine m15 -iface enp216s0 ./allreduce-test cpu >> ~/single.txt
+sleep 30s;
+done
