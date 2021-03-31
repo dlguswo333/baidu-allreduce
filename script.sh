3d88539ef457a273a63a9cee7bb160c44350ad28
dlguswo333 <dlguswo333@skku.edu>
2021-03-31 07:05:13 +0000

    init script.sh for intel-concurrent

--- script.sh
+++ script.sh
@@ -1,28 +1,21 @@
-printf "41\n\n" >> ~/thread.txt
-for i in {1..10}
+printf "31\n\n" >> ~/thread.txt
+for i in {1..20}
 do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:4,tb3-ib:1 allreduce-test cpu >> ~/thread.txt
+mpirun -n 4 -iface enp216s0 -machine m31 ./allreduce-test cpu
 sleep 30s;
 done
 
-printf "32\n\n" >> ~/thread.txt
-for i in {1..10}
+printf "22\n\n" >> ~/thread.txt
+for i in {1..20}
 do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:3,tb3-ib:2 allreduce-test cpu >> ~/thread.txt
+mpirun -n 4 -iface enp216s0 -machine m22 ./allreduce-test cpu
 sleep 30s;
 done
 
-printf "23\n\n" >> ~/thread.txt
-for i in {1..10}
+printf "13\n\n" >> ~/thread.txt
+for i in {1..20}
 do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:2,tb3-ib:3 allreduce-test cpu >> ~/thread.txt
-sleep 30s;
-done
-
-printf "14\n\n" >> ~/thread.txt
-for i in {1..10}
-do
-mpirun --allow-run-as-root -np 5 -mca btl_tcp_if_exclude eno0,eno1 -mca plm_rsh_args "-p 3333" -H localhost:1,tb3-ib:4 allreduce-test cpu >> ~/thread.txt
+mpirun -n 4 -iface enp216s0 -machine m13 ./allreduce-test cpu
 sleep 30s;
 done
 
