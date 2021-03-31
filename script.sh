40ce83d9f9ab45852fa66378a4f1fb4071c2271b
dlguswo333 <dlguswo333@skku.edu>
2021-03-31 07:06:45 +0000

    fix script.sh to add stream redirection

--- script.sh
+++ script.sh
@@ -1,21 +1,21 @@
 printf "31\n\n" >> ~/thread.txt
 for i in {1..20}
 do
-mpirun -n 4 -iface enp216s0 -machine m31 ./allreduce-test cpu
+mpirun -n 4 -iface enp216s0 -machine m31 ./allreduce-test cpu >> /thread.txt
 sleep 30s;
 done
 
 printf "22\n\n" >> ~/thread.txt
 for i in {1..20}
 do
-mpirun -n 4 -iface enp216s0 -machine m22 ./allreduce-test cpu
+mpirun -n 4 -iface enp216s0 -machine m22 ./allreduce-test cpu >> /thread.txt
 sleep 30s;
 done
 
 printf "13\n\n" >> ~/thread.txt
 for i in {1..20}
 do
-mpirun -n 4 -iface enp216s0 -machine m13 ./allreduce-test cpu
+mpirun -n 4 -iface enp216s0 -machine m13 ./allreduce-test cpu >> /thread.txt
 sleep 30s;
 done
 
